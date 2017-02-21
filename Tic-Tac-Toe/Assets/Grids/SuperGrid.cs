58c83150b6f32348d0cb629ad3dfeead379c5441
Richard Greaves <roboritch@gmail.com>
2017-02-21 12:17:23 -0800

    fist commit

---
+++ Tic-Tac-Toe/Assets/Grids/SuperGrid.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SuperGrid : MonoBehaviour {
+
+	#region prefabs
+	[SerializeField]
+	GameObject gameGrid;
+
+
+	#endregion
+
+	private static int numbSpaces = 3;
+	private TicTacToeGrid[,] games = new TicTacToeGrid[numbSpaces,numbSpaces];
+		
+	/// <summary>
+	/// creates the 9 game grids from the prefab and places them in games
+	/// </summary>
+	private void spawnGameGrids() {
+		for (int x = 0; x < numbSpaces; x++) {
+			for (int y = 0; y < numbSpaces; y++) {
+				games[x,y] = Instantiate(gameGrid).GetComponent<TicTacToeGrid>();
+				
+				//games[x, y].GetComponent<RectTransform>().anchoredPosition;
+
+
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		spawnGameGrids();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Tic-Tac-Toe/Assets/Grids/TicTacToeGrid.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TicTacToeGrid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//TODO: init grid and buttons
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
